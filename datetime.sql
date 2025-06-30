7706e9dea22027d3de760e1e990514b445eef047
Agnimitra-cloud <110715880+Agnimitra-cloud@users.noreply.github.com>
2025-06-30 12:05:15 -0700

    Update datetime.sql

--- datetime.sql
+++ datetime.sql
@@ -10,158 +10,133 @@ CREATE OR ALTER PROCEDURE dbo.CalculateWorkingHours
 AS
 BEGIN
     SET NOCOUNT ON;
-    
-    -- Validate input dates
+
+    -- Input validations
     IF @StartDate > @EndDate
     BEGIN
-        SELECT 0.0 AS TotalWorkingHours, 
-               'Start date must be before end date' AS Message;
+        SELECT 0.0 AS TotalWorkingHours, 'Start date must be before end date' AS Message;
         RETURN;
-    END
-    
+    END;
+
+    IF @DailyStartTime >= @DailyEndTime
+    BEGIN
+        SELECT 0.0 AS TotalWorkingHours, 'Start time must be earlier than end time' AS Message;
+        RETURN;
+    END;
+
     DECLARE @TotalHours DECIMAL(10,2) = 0.0;
     DECLARE @CurrentDate DATETIME = @StartDate;
-    
-    -- Temporary table to store working days
+
+    -- Create temporary table
     CREATE TABLE #WorkingDays (
         Date DATE,
         WorkingHours DECIMAL(5,2)
     );
-    
-    -- Process each day in the date range
+
+    -- Process each day
     WHILE @CurrentDate <= @EndDate
     BEGIN
         DECLARE @DayOfWeek INT = DATEPART(WEEKDAY, @CurrentDate);
-        DECLARE @DayOfMonth INT = DAY(@CurrentDate);
-        DECLARE @FirstDayOfMonth DATE = DATEFROMPARTS(YEAR(@CurrentDate), MONTH(@CurrentDate), 1);
-        DECLARE @IsFirstOrSecondSaturday BIT = 0;
-        
-        -- Check if it's the 1st or 2nd Saturday of the month
-        IF @DayOfWeek = 7 -- Saturday
+        DECLARE @IsWeekend BIT = 0;
+        DECLARE @IsNonWorkingSaturday BIT = 0;
+
+        -- Detect Sunday
+        IF @DayOfWeek = 1
+            SET @IsWeekend = 1;
+
+        -- Check if it's a Saturday
+        IF @DayOfWeek = 7
         BEGIN
-            DECLARE @FirstSaturday DATE = DATEADD(
-                DAY, 
-                (7 - DATEPART(WEEKDAY, @FirstDayOfMonth) + 1) % 7, 
-                @FirstDayOfMonth
-            );
-            
-            DECLARE @SecondSaturday DATE = DATEADD(DAY, 7, @FirstSaturday);
-            
-            IF CAST(@CurrentDate AS DATE) = @FirstSaturday 
-               OR CAST(@CurrentDate AS DATE) = @SecondSaturday
-                SET @IsFirstOrSecondSaturday = 1;
-        END
-        
-        -- Calculate hours for working days
-        IF @DayOfWeek <> 1 -- Not Sunday
-           AND @IsFirstOrSecondSaturday = 0 -- Not 1st or 2nd Saturday
+            DECLARE @FirstDayOfMonth DATE = DATEFROMPARTS(YEAR(@CurrentDate), MONTH(@CurrentDate), 1);
+            DECLARE @SaturdayCounter INT = 0;
+            DECLARE @CheckDate DATE = @FirstDayOfMonth;
+
+            WHILE MONTH(@CheckDate) = MONTH(@CurrentDate)
+            BEGIN
+                IF DATEPART(WEEKDAY, @CheckDate) = 7 -- Saturday
+                BEGIN
+                    SET @SaturdayCounter += 1;
+                    IF @CheckDate = CAST(@CurrentDate AS DATE)
+                        BREAK;
+                END;
+                SET @CheckDate = DATEADD(DAY, 1, @CheckDate);
+            END;
+
+            IF @SaturdayCounter > 2 -- 3rd, 4th, etc.
+                SET @IsNonWorkingSaturday = 1;
+        END;
+
+        -- Skip non-working days
+        IF @IsWeekend = 0 AND @IsNonWorkingSaturday = 0
         BEGIN
             DECLARE @DayStart DATETIME;
             DECLARE @DayEnd DATETIME;
-            DECLARE @HoursForDay DECIMAL(5,2);
-            
-            -- Handle start date (might be partial day)
+
+            -- Determine working window for the day
+            SET @DayStart = DATETIMEFROMPARTS(
+                YEAR(@CurrentDate), MONTH(@CurrentDate), DAY(@CurrentDate),
+                DATEPART(HOUR, @DailyStartTime), DATEPART(MINUTE, @DailyStartTime),
+                DATEPART(SECOND, @DailyStartTime), 0
+            );
+
+            SET @DayEnd = DATETIMEFROMPARTS(
+                YEAR(@CurrentDate), MONTH(@CurrentDate), DAY(@CurrentDate),
+                DATEPART(HOUR, @DailyEndTime), DATEPART(MINUTE, @DailyEndTime),
+                DATEPART(SECOND, @DailyEndTime), 0
+            );
+
+            -- Adjust if first or last day
             IF CAST(@CurrentDate AS DATE) = CAST(@StartDate AS DATE)
-            BEGIN
-                -- Use the later of start date/time or daily start time
-                SET @DayStart = CASE 
-                    WHEN CAST(@StartDate AS TIME) > @DailyStartTime THEN @StartDate
-                    ELSE DATETIMEFROMPARTS(
-                        YEAR(@CurrentDate), MONTH(@CurrentDate), DAY(@CurrentDate),
-                        DATEPART(HOUR, @DailyStartTime), DATEPART(MINUTE, @DailyStartTime), 
-                        DATEPART(SECOND, @DailyStartTime), 0
-                    )
-                END;
-                
-                -- End time is the earlier of end date/time or daily end time
-                SET @DayEnd = CASE 
-                    WHEN CAST(@EndDate AS DATE) = CAST(@StartDate AS DATE) 
-                         AND CAST(@EndDate AS TIME) < @DailyEndTime THEN @EndDate
-                    ELSE DATETIMEFROMPARTS(
-                        YEAR(@CurrentDate), MONTH(@CurrentDate), DAY(@CurrentDate),
-                        DATEPART(HOUR, @DailyEndTime), DATEPART(MINUTE, @DailyEndTime), 
-                        DATEPART(SECOND, @DailyEndTime), 0
-                    )
-                END;
-            END
-            -- Handle end date (might be partial day)
-            ELSE IF CAST(@CurrentDate AS DATE) = CAST(@EndDate AS DATE)
-            BEGIN
-                -- Start at daily start time
-                SET @DayStart = DATETIMEFROMPARTS(
-                    YEAR(@CurrentDate), MONTH(@CurrentDate), DAY(@CurrentDate),
-                    DATEPART(HOUR, @DailyStartTime), DATEPART(MINUTE, @DailyStartTime), 
-                    DATEPART(SECOND, @DailyStartTime), 0
-                );
-                
-                -- End at the earlier of end time or daily end time
-                SET @DayEnd = CASE 
-                    WHEN CAST(@EndDate AS TIME) < @DailyEndTime THEN @EndDate
-                    ELSE DATETIMEFROMPARTS(
-                        YEAR(@CurrentDate), MONTH(@CurrentDate), DAY(@CurrentDate),
-                        DATEPART(HOUR, @DailyEndTime), DATEPART(MINUTE, @DailyEndTime), 
-                        DATEPART(SECOND, @DailyEndTime), 0
-                    )
-                END;
-            END
-            -- Full working days in between
-            ELSE
-            BEGIN
-                SET @DayStart = DATETIMEFROMPARTS(
-                    YEAR(@CurrentDate), MONTH(@CurrentDate), DAY(@CurrentDate),
-                    DATEPART(HOUR, @DailyStartTime), DATEPART(MINUTE, @DailyStartTime), 
-                    DATEPART(SECOND, @DailyStartTime), 0
-                );
-                
-                SET @DayEnd = DATETIMEFROMPARTS(
-                    YEAR(@CurrentDate), MONTH(@CurrentDate), DAY(@CurrentDate),
-                    DATEPART(HOUR, @DailyEndTime), DATEPART(MINUTE, @DailyEndTime), 
-                    DATEPART(SECOND, @DailyEndTime), 0
-                );
-            END
-            
-            -- Calculate hours for the day (subtract lunch break if full day)
-            SET @HoursForDay = DATEDIFF(MINUTE, @DayStart, @DayEnd)/60.0;
-            
-            -- Subtract lunch break if working at least 6 hours (full day)
+                SET @DayStart = IIF(@StartDate > @DayStart, @StartDate, @DayStart);
+
+            IF CAST(@CurrentDate AS DATE) = CAST(@EndDate AS DATE)
+                SET @DayEnd = IIF(@EndDate < @DayEnd, @EndDate, @DayEnd);
+
+            -- Calculate hours
+            DECLARE @HoursForDay DECIMAL(5,2) = DATEDIFF(MINUTE, @DayStart, @DayEnd) / 60.0;
+
             IF @HoursForDay >= 6.0
-                SET @HoursForDay = @HoursForDay - @LunchBreakHours;
-                
-            -- Ensure non-negative hours
+                SET @HoursForDay -= @LunchBreakHours;
+
             IF @HoursForDay > 0
             BEGIN
                 INSERT INTO #WorkingDays (Date, WorkingHours)
                 VALUES (CAST(@CurrentDate AS DATE), @HoursForDay);
-                
-                SET @TotalHours = @TotalHours + @HoursForDay;
-            END
-        END
-        
-        -- Move to next day (without time component)
-        SET @CurrentDate = DATEADD(DAY, 1, CAST(@CurrentDate AS DATE));
-    END
-    
-    -- Return results
+
+                SET @TotalHours += @HoursForDay;
+            END;
+        END;
+
+        SET @CurrentDate = DATEADD(DAY, 1, @CurrentDate);
+    END;
+
+    -- Final output
     SELECT 
         @TotalHours AS TotalWorkingHours,
         @StartDate AS StartDate,
         @EndDate AS EndDate;
-    
-    -- Optionally return daily breakdown
+
+    -- Optional breakdown
     SELECT 
         Date,
         WorkingHours,
         DATENAME(WEEKDAY, Date) AS DayName,
         CASE 
-            WHEN DAY(Date) IN (1, 2, 3, 4, 5, 6, 7) 
-                 AND DATEPART(WEEKDAY, Date) = 7 THEN '1st Week Saturday'
-            WHEN DAY(Date) IN (8, 9, 10, 11, 12, 13, 14) 
-                 AND DATEPART(WEEKDAY, Date) = 7 THEN '2nd Week Saturday'
+            WHEN DATEPART(WEEKDAY, Date) = 7 THEN 
+                CASE 
+                    WHEN DAY(Date) BETWEEN 1 AND 7 THEN '1st Week Saturday'
+                    WHEN DAY(Date) BETWEEN 8 AND 14 THEN '2nd Week Saturday'
+                    ELSE NULL
+                END
             ELSE NULL
         END AS SpecialDayType
     FROM #WorkingDays
     ORDER BY Date;
-    
+
     DROP TABLE #WorkingDays;
-END
+END;
 GO
+
+EXEC dbo.CalculateWorkingHours 
+    @StartDate = '2025-06-01 09:30:00',
+    @EndDate = '2025-06-10 16:30:00';
